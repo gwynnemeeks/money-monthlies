4ac5307d1c3dd27cb35f7c5e0e946bb43d4cf1b7
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-19 11:15:57 -0500

    wip expenses

---
+++ src/components/Expenses.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Monthly Expenses
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["expenses"],
+  data() {
+    return {
+      name: "",
+      amount: null,
+    };
+  },
+};
+</script>
+
+<style></style>
