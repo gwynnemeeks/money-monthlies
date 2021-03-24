5dd7a20bc6ab8afd9af69f705b9239b9d5cf69e9
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-24 11:12:59 -0500

    added vue apex charts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
+import VueApexCharts from "vue-apexcharts";
+
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 
 Vue.config.productionTip = false
 
