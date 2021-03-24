318eba3d65a17451a3c86744f353f1c17423c930
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-24 11:21:33 -0500

    wip pie chart template

---
+++ src/components/PieChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card height="100%">
+    <v-card-title class="purple--text">Monthly Expense Breakdown</v-card-title>
+    <apexchart type="pie" :options="options" :series="series" height="300" />
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
