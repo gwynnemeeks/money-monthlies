98d011329a1223db00d413431ee87bbab93d0cf7
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-24 11:27:34 -0500

    wip pie chart script

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -6,7 +6,27 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["expenses", "totalAmount"],
+  computed: {
+    percentageReport() {
+      return this.expenses.map((e) => {
+        return {
+          name: e.name,
+          percentage: ((e.amount / this.totalAmount) * 100).toFixed(2),
+        };
+      });
+    },
+    series() {
+      return this.expenses.map((e) => e.amount);
+    },
+    options() {
+      return {
+        labels: this.expenses.map((e) => e.name),
+      };
+    },
+  },
+};
 </script>
 
 <style></style>
